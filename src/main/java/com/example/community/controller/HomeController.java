dec3c919b6874bd93700980a08c2251e3a33d9cd
Nicolson <nks111474@163.com>
2022-12-25 16:45:52 +0800

    The verification code and image are generated randomly, and the verification code information is saved using session

--- src/main/java/com/example/community/controller/HomeController.java
+++ src/main/java/com/example/community/controller/HomeController.java
@@ -5,18 +5,25 @@ import com.example.community.entity.Page;
 import com.example.community.entity.User;
 import com.example.community.service.DiscussPostService;
 import com.example.community.service.UserService;
+import com.example.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Controller
+@RequestMapping("/alpha")
 public class HomeController {
     @Autowired
     private UserService userService;
@@ -45,5 +52,45 @@ public class HomeController {
     }
 
 
+    //cookie实例
+    @RequestMapping(path="/cookie/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response)
+    {
+        //创建Cookie
+        Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+        //设置有效范围
+        cookie.setPath("community/alpha");
+        //设置cookie的生存时间
+        cookie.setMaxAge(60 * 10);//十分钟
+        response.addCookie(cookie);
+        return "set cookie";
+    }
+
+    @RequestMapping(path="/cookie/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") String code)
+    {
+        System.out.println(code);
+        return "get";
+    }
+
+    //session示例
+    @RequestMapping(path = "/session/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session){
+        session.setAttribute("id","1");
+        session.setAttribute("name","test");
+        return "set session";
+    }
+    @RequestMapping(path = "/session/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session){
+        Object id = session.getAttribute("id");
+        Object name = session.getAttribute("name");
+        System.out.println(id);
+        System.out.println(name);
+        return "set session";
+    }
 
 }
