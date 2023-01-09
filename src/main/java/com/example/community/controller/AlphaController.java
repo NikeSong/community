19757885082d91b641776e37347e5924a3d664e2
Nicolson <nks111474@163.com>
2023-01-09 21:20:01 +0800

    add:json process util

--- src/main/java/com/example/community/controller/AlphaController.java
+++ src/main/java/com/example/community/controller/AlphaController.java
@@ -1,4 +1,18 @@
 package com.example.community.controller;
 
+import com.example.community.util.CommunityUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class AlphaController {
+
+
+    @RequestMapping(path = "/ajax",method = RequestMethod.POST)
+    @ResponseBody
+    public String testAjax(String name,String age){
+        System.out.println(name);
+        System.out.println(age);
+        return CommunityUtil.getJSONString(0,"success");
+    }
 }
