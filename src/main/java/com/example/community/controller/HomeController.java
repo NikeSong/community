126296c99e3610b8f73845f87ee75415811e9e5d
Nicolson <nks111474@163.com>
2023-01-15 17:52:32 +0800

    add: unified process the error condition

--- src/main/java/com/example/community/controller/HomeController.java
+++ src/main/java/com/example/community/controller/HomeController.java
@@ -93,4 +93,8 @@ public class HomeController {
         return "set session";
     }
 
+    @RequestMapping(path = "/error",method = RequestMethod.GET)
+    public String getErrorPage() {
+        return "/error/500";
+    }
 }
